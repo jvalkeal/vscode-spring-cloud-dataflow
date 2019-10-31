05b04855bc0a78fbaf49e78923fcd12f820c87f2
Janne Valkealahti <janne.valkealahti@gmail.com>
2019-10-31 16:29:16 +0000

    stash

--- ci/scripts/vscode-spring-cloud-dataflow-publish.sh
+++ ci/scripts/vscode-spring-cloud-dataflow-publish.sh
@@ -4,3 +4,5 @@ cd git-repo
 
 envsubst < ~/.npmrc > ~/.npmrc.tmp && mv  ~/.npmrc.tmp ~/.npmrc
 npm install && npm run build
+mkdir -p org/springframework/cloud/dataflow
+mv *.vsix org/springframework/cloud/dataflow/
