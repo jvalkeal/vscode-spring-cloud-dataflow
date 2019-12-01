1192df46ab4c4f9ddb530f2cd11dac1c2db7d1e1
Janne Valkealahti <janne.valkealahti@gmail.com>
2019-12-01 09:45:07 +0000

    stash

--- scdfwebview.js
+++ scdfwebview.js
@@ -1,11 +1,11 @@
 var fs = require('fs');
 var cpFile = require('cp-file');
 var resolveFile = require('resolve-file');
-var bundle = resolveFile.file('node_modules/@pivotal-tools/vscode-spring-cloud-dataflow-stream-webview/dist/ScdfStreamWebviewWebpack/bundle.js');
+var bundle = resolveFile.file('node_modules/@pivotal-tools/vscode-spring-cloud-dataflow-webview-flo/dist/vscode-spring-cloud-dataflow-webview-flo-bundle/bundle.js');
 var dir = 'dist/webview';
 
 if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
+    fs.mkdirSync(dir, {recursive: true});
 }
 
 (async () => {
